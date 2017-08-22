0fb8173bcff3375a73c4b4b83a7aa015d0900b1a
PeckTonium <30939668+PeckTonium@users.noreply.github.com>
2017-08-22 15:14:53 -0700

    Update nextbuildver.py

--- engagement/email/nextbuildver.py
+++ engagement/email/nextbuildver.py
@@ -5,7 +5,7 @@ nextbuildver.py
 Print the next build version number of the specified build artifact.
 
 The artifact_name will be used as the basis to find the correct item to version.  If the name
-has dots in it. the last one will be expected to be the file extension, which will occur after
+has dots in it. the last dot will be expected to be the file extension, which will occur after
 the version number in the final file.  For example:
   abc.tgz -> abc_0.0.5-rc.4.tgz
   atgz -> atgz_0.0.5-rc.4
@@ -13,8 +13,8 @@ the version number in the final file.  For example:
 Note: the artifact_name must have the version following the last underscore, i.e a_this_1.2.3.tgz
 
 The bucket_dir directory in the bucket_name s3 bucket will be searched for all files
-starting prefixed with artifact_name ($artifact_name*, without the extension if present).  The
-highest version found will be incremented and printed to stdout
+prefixed with artifact_name ($artifact_name*, without the extension if present).  The highest version
+found will be incremented and printed to stdout
 
 If --major is present, then the major version will be incremented and the rest will be set to zero
   with a new prerelease version
@@ -34,9 +34,12 @@ import boto3
 import semver
 
 parser = argparse.ArgumentParser(description='Find next version number')
-parser.add_argument('--artifact_name', required=True, help='the base of the artifact to version')
-parser.add_argument('--bucket_name', required=True, help='the name of the s3 bucket that the artifacts are in')
-parser.add_argument('--bucket_dir', required=True, help='the directory inside the s3 bucket where artifacts are')
+parser.add_argument('--artifact_name', required=True,
+                    help='the base of the artifact to version')
+parser.add_argument('--bucket_name', required=True,
+                    help='the name of the s3 bucket that the artifacts are in')
+parser.add_argument('--bucket_dir', required=True,
+                    help='the directory inside the s3 bucket where artifacts are')
 parser.add_argument('--major', action='store_true')
 parser.add_argument('--minor', action='store_true')
 parser.add_argument('--patch', action='store_true')
@@ -70,8 +73,20 @@ bucket = s3.Bucket(args.bucket_name)
 obj_iterator = bucket.objects.filter(Prefix=artifact_prefix)
 item_names = sorted([x.key for x in obj_iterator])
 
+if not len(item_names):
+  # New file to begin versioning
+  new_ver = semver.format_version(0, 0, 1)
+  new_ver = semver.bump_prerelease(new_ver)
+  print new_ver
+  exit(0)
+
 ver_index = item_names[-1].rfind("_")
 highest_existing_ver= item_names[-1][ver_index:]
+
+print "Have prefix %s and name %s" % (artifact_prefix, item_names[-1])
+if item_names[-1][:ver_index] != artifact_prefix:
+  print "Error: Found no versioned files based on %s" % artifact_prefix
+  exit(1)
 
 if highest_existing_ver.startswith("_"):
   highest_existing_ver = highest_existing_ver[1:]
